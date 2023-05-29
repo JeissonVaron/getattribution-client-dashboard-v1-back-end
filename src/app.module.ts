6761704300ae41664476fa75ec4523498c8e1827
Jeisson Rodríguez <jeissonrodriguez.varon@gmail.com>
2023-05-29 12:03:08 -0500

    add user controler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
