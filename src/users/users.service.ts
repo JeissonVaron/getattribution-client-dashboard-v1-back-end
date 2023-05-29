6279cb25869dd802dcaf657af922a7a49890d7a6
Alex1703 <p.a.rodriguez1102@gmail.com>
2023-05-29 11:08:34 -0500

    feat(auth): Implement JWT authentication with login endpoint
    - Add User entity with validation
    - Create Auth and Users modules
    - Implement AuthService with JWT token generation
    - Implement UsersService with user lookup
    - Configure AuthModule with Passport and JWT
    - Create AuthController with login endpoint
    - Implement LocalAuthGuard and JwtStrategy for authentication

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+    private users: User[] = [];
+
+    async findOne(username: string): Promise<User | undefined> {
+      return this.users.find(user => user.username === username);
+    }
+}
